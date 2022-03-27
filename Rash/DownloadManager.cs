3dcbaad223a247dc198e2f3dae4fd3cf892bc60d
Ryhon <ryhon0@gmail.com>
2022-03-27 11:23:09 +0200

    Move BytesToString to an extension function

--- Rash/DownloadManager.cs
+++ Rash/DownloadManager.cs
@@ -132,18 +132,6 @@ public class Downloader
 		}
 		return next;
 	}
-
-	public static string BytesToString(long byteCount)
-	{
-		string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
-		if (byteCount == 0)
-			return "0" + suf[0];
-		long bytes = Math.Abs(byteCount);
-		int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-		double num = Math.Round(bytes / Math.Pow(1024, place), 1);
-		return (Math.Sign(byteCount) * num).ToString() + suf[place];
-	}
-
 }
 
 public enum DownloaderState
