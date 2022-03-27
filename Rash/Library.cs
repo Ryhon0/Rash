18f29767fdb2e6fe52624fdbc79e44abba34656b
Ryhon <ryhon0@gmail.com>
2022-03-27 11:28:06 +0200

    Library preparation

---
+++ Rash/Library.cs
@@ -0,0 +1,18 @@
+namespace Rash;
+using SharpItch;
+
+public static class Library
+{
+
+}
+
+public class LibraryInfo
+{
+	public string Path { get; set; }
+
+}
+
+public class LibraryGameInfo
+{
+		
+}
