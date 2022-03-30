b778f7f5bad015ee8e6a3e58f6d6a225f367e2e0
Ryhon <ryhon0@gmail.com>
2022-03-30 23:26:15 +0200

    GetOwnedKeysV1

---
+++ SharpItch/API/V1/MyOwnedKeys.cs
@@ -0,0 +1,18 @@
+namespace SharpItch;
+
+partial class Itch
+{
+	/// <summary>
+	/// Same as V2, but returns 500 keys per page instead of 50.
+	/// </summary>
+	/// <param name="page"></param>
+	/// <returns></returns>
+	public async Task<OwnedKeysResult> GetOwnedKeysV1(long page = 1)
+	{
+		return await new RequestBuilder(BuildV1ApiURL("my-owned-keys"))
+			.AddToken(this)
+			.AddLong("page", page)
+			.AddConverter(new V1DateConverter())
+			.Get<OwnedKeysResult>(HttpClient);
+	}
+}
