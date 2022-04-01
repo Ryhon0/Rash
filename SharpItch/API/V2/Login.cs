2825f2b756e74a7eedb2f3157913481102a16cae
Ryhon <ryhon0@gmail.com>
2022-04-01 14:11:30 +0200

    TOTP

--- SharpItch/API/V2/Login.cs
+++ SharpItch/API/V2/Login.cs
@@ -1,10 +1,40 @@
-namespace SharpItch;
-
-using System.Text.Json;
-using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
-public class ItchLoginResponse : ItchResponse
+namespace SharpItch;
+
+public partial class Itch
+{
+	public async Task<ItchLoginResponse> Login(string username, string password,
+		string recaptcha_response = null, bool force_recaptcha = false,
+		string source = "desktop")
+	{
+		return await new RequestBuilder(BaseAPIURLV2 + "login")
+			.AddString("source", source)
+			.AddString("username", username)
+			.AddString("password", password)
+			.AddStringIfNotEmpty("recaptcha_response", recaptcha_response)
+			.AddBoolIfTrue("force_recaptcha", force_recaptcha)
+			.Post<ItchLoginResponse>(HttpClient);
+	}
+
+	public async Task<TOTPVerifyResponse> TOTPVerify(string token, string code)
+	{
+		return await new RequestBuilder(BaseAPIURLV2 + "totp/verify")
+			.AddString("token", token)
+			.AddString("code", code)
+			.Post<TOTPVerifyResponse>(HttpClient);
+	}
+}
+
+public class TOTPVerifyResponse : ItchResponse
+{
+	[JsonPropertyName("key")]
+	public ItchAPIKey Key { get; set; }
+	[JsonPropertyName("cookie")]
+	public Dictionary<string, string> Cookies { get; set; }
+}
+
+public class ItchLoginResponse : TOTPVerifyResponse
 {
 	[JsonPropertyName("recaptcha_needed")]
 	public bool RecaptchaNeeded { get; set; }
@@ -14,11 +44,6 @@ public class ItchLoginResponse : ItchResponse
 	public bool TOTPNeeded { get; set; }
 	[JsonPropertyName("token")]
 	public string Token { get; set; }
-
-	[JsonPropertyName("key")]
-	public ItchAPIKey Key { get; set; }
-	[JsonPropertyName("cookie")]
-	public Dictionary<string, string> Cookies { get; set; }
 }
 
 public class ItchAPIKey
@@ -35,20 +60,4 @@ public class ItchAPIKey
 	public DateTime UpdatedAt { get; set; }
 	[JsonPropertyName("source_version")]
 	public string SourceVersion { get; set; }
-}
-
-public partial class Itch
-{
-	public async Task<ItchLoginResponse> Login(string username, string password,
-		string recaptcha_response = null, bool force_recaptcha = false,
-		string source = "desktop")
-	{
-		return await new RequestBuilder(BaseAPIURLV2 + "login")
-			.AddString("source", source)
-			.AddString("username", username)
-			.AddString("password", password)
-			.AddStringIfNotEmpty("recaptcha_response", recaptcha_response)
-			.AddBoolIfTrue("force_recaptcha", force_recaptcha)
-			.Post<ItchLoginResponse>(HttpClient);
-	}
 }
